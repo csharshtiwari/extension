390fb86d40976de2e07a4e77a8c00d307b8b9b57
Harsh Tiwari <harsh2018cs176abesit@gmail.com>
2020-10-02 17:26:58 +0530

    Update eventPage.js

--- eventPage.js
+++ eventPage.js
@@ -5,3 +5,38 @@ var contextMenuitem = {
 };
 
 chrome.contextMenus.create(contextMenuitem);
+
+	function isInt(value){
+		return !isNaN(value) && 
+				parseInt(Number(value))== value &&
+				!isNaN(parseInt(value,10));
+	}
+
+chrome.contextMenus.onClicked.addListener(function(clickData){
+	if(clickData.menuItemId == 'spendMoney' && clickData.selectionText){
+		if(isInt(clickData.selectionText)){
+			chrome.storage.sync.get(['total','limit'], function(budget){
+				var newTotal = 0;
+				if(budget.total){
+					newTotal += parseInt(budget.total);
+
+				}
+				newTotal += parseInt(clickData.selectionText);
+				chrome.storage.sync.set({'total':newTotal}, function(){
+					if(newTotal >= budget.limit){
+						var notifiOptions = {
+						type: 'basic',
+						iconUrl: 'icons.png',
+						title: 'limit reached'
+						message: 'HEY! You have reached your limit'
+					};
+					chrome.notifications.create('limitNotif',notifiOptions);
+					}
+				});
+			});
+		}
+	}
+});
+chrome.storage.onChanged.addListener(function(changes, storageName){
+	chrome.browserAction.setBadgeText({'text': changes.total.newValue.toString()});
+});
