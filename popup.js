31fa4e13ae8d4113a947d4997274d37cbf5ab0f1
Harsh Tiwari <harsh2018cs176abesit@gmail.com>
2020-10-02 15:37:31 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,12 +1,13 @@
 $(function(){
-	chrome.storage.sync.get('total',function(budget){
+	chrome.storage.sync.get(['total' , 'limit'],function(budget){
 		$('#total').text(budget.total);
+		$('#limit').text(budget.limit)
 	})
 
 
 
 	$("#spendAmount").click(function(){
-		chrome.storage.sync.get('total', function(budget){
+		chrome.storage.sync.get(['total', 'limit'], function(budget){
 			var newTotal = 0;
 			if(budget.total){
 				newTotal += parseInt(budget.total);
@@ -16,7 +17,17 @@ $(function(){
 				newTotal += parseInt(amount);
 			} 
 
-			chrome.storage.sync.set({'total': newTotal});
+			chrome.storage.sync.set({'total': newTotal},function(){
+				if(amount && newTotal >= budget.limit){
+					var notifiOptions = {
+						type: 'basic',
+						iconUrl: 'icons.png',
+						title: 'limit reached'
+						message: 'HEY! You have reached your limit'
+					};
+					chrome.notifications.create('limitNotif',notifiOptions);
+				}	
+			});
 
 			$('total').text(newTotal);
 			$('#amount').val('');
